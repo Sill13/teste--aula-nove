90e2f114d4c09baaf2a4d0db4b4c7acf81073a2c
Sill13 <94568861+Sill13@users.noreply.github.com>
2021-11-17 18:09:35 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var bola;
+function setup() {
+  createCanvas(400,400);
+  bola= createSprite (200,200,20,20)
+bola.shapeColor= "white";
+}
+
+function draw() 
+{
+  background(30);
+drawSprites ();
+}
+
+
+
+
